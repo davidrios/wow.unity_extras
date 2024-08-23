14d353f7e649da0395d680562d699525708b6385
David Rios <david.rios.gomes@gmail.com>
2024-08-23 11:57:30 -0300

    Improve copying script

--- misc_scripts/copy_exported.py
+++ misc_scripts/copy_exported.py
@@ -9,6 +9,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("source_dir")
     parser.add_argument("dest_dir")
+    parser.add_argument("-v", action="store_true", help="Show more logs.")
     parser.add_argument("--fbx-dir")
     parser.add_argument(
         "--replace", action="store_true", help="Replace all files in destination."
@@ -35,8 +36,8 @@ def main():
 
     def copy_to(source: Path, dest: Path, force_replace=False):
         if not source.is_file() or (
-            (include is not None and not include.match(str(source))) or
-            (exclude is not None and exclude.match(str(source)))
+            (include is not None and not include.match(str(source)))
+            or (exclude is not None and exclude.match(str(source)))
         ):
             return
 
@@ -46,13 +47,53 @@ def main():
             if not args.dry_run:
                 shutil.copy(source, dest)
         else:
-            print("[!]", source, "-", dest)
+            if args.v:
+                print("[!]", source, "-", dest)
 
     fbx_dir = Path(args.fbx_dir) if args.fbx_dir is not None else None
     if fbx_dir is not None and not fbx_dir.is_dir():
         fbx_dir = None
 
-    fbx_extras = []
+    fbx_extras = set()
+
+    def obj_has_fbx(relative_source_item: Path, dest_file: Path):
+        if fbx_dir is None:
+            return False
+
+        source_fbx = fbx_dir / relative_source_item.with_suffix(".fbx")
+        if not source_fbx.is_file():
+            return False
+
+        fbx_extras.add(
+            (
+                source_fbx,
+                dest_dir / source_fbx.relative_to(fbx_dir),
+            )
+        )
+
+        extra_source = source_fbx.with_name(
+            source_fbx.name.replace(".fbx", "_anim.fbx")
+        )
+        if (
+            args.anims
+            and not len(list(dest_file.parent.glob(f"{source_fbx.stem}_*.anim")))
+            and extra_source.is_file()
+        ):
+            fbx_extras.add((extra_source, dest_file.with_name(extra_source.name), True))
+
+        for extra_file in source_fbx.parent.glob("*.*"):
+            if not re.match(r".+\.(png|json)$", extra_file.name):
+                continue
+
+            fbx_extras.add(
+                (
+                    extra_file,
+                    dest_dir / extra_file.relative_to(fbx_dir),
+                    extra_file.suffix == ".json",
+                )
+            )
+
+        return True
 
     for source_item in source_dir.glob("**/*"):
         if source_item.suffix == ".bin":
@@ -62,41 +103,26 @@ def main():
         dest_file = dest_dir / relative_source_item
 
         if source_item.suffix == ".gltf":
-            if fbx_dir is None:
-                continue
-
-            source_fbx = fbx_dir / relative_source_item.with_suffix(".fbx")
-            if not source_fbx.is_file():
-                continue
-
-            fbx_extras.append(
-                (
-                    source_fbx,
-                    dest_dir / source_fbx.relative_to(fbx_dir),
-                )
-            )
-
-            extra_source = source_fbx.with_name(
-                source_fbx.name.replace(".fbx", "_anim.fbx")
-            )
-            if args.anims and extra_source.is_file():
-                fbx_extras.append(
-                    (extra_source, dest_file.with_name(extra_source.name), True)
-                )
-
-            for extra_file in source_fbx.parent.glob('*.*'):
-                if not re.match(r'.+\.(png|json)$', extra_file.name):
-                    continue
-
-                fbx_extras.append(
-                    (
-                        extra_file,
-                        dest_dir / extra_file.relative_to(fbx_dir),
-                        extra_file.suffix == '.json',
-                    )
-                )
-
+            obj_has_fbx(relative_source_item, dest_file)
             continue
+
+        if (
+            source_item.suffix == ".obj"
+            or source_item.suffix == ".mtl"
+            or source_item.name.endswith("_bones.json")
+        ):
+            do_continue = False
+            for suffix in (".phys", "_bones"):
+                if obj_has_fbx(
+                    relative_source_item.with_name(
+                        relative_source_item.name.replace(suffix, "")
+                    ),
+                    dest_file,
+                ):
+                    do_continue = True
+
+            if do_continue:
+                continue
 
         copy_to(
             source_item,
