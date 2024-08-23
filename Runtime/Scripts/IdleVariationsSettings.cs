dbf6da091fbbb450f3e4ea7830f18d4416996593
David Rios <david.rios.gomes@gmail.com>
2024-08-22 22:30:01 -0300

    Fix default value

--- Runtime/Scripts/IdleVariationsSettings.cs
+++ Runtime/Scripts/IdleVariationsSettings.cs
@@ -5,7 +5,7 @@ namespace WoWUnityExtras
     [CreateAssetMenu(fileName = "IdleVariationsSettings", menuName = "wow.unity_extras/IdleVariationsSettings", order = 1)]
     public class IdleVariationsSettings : ScriptableObject
     {
-        public float variationChance = 0.1f;
+        public float variationChance = 0.01f;
         public float checkInterval = 0.1f;
     }
 }
